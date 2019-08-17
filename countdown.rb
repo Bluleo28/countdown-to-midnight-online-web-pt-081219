f51ee7a488e50649be51bc925f0048c0d907d13e
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-17 01:13:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,11 +7,11 @@ end
   puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(num)
+# def countdown_with_sleep(num)
 
-while num > 0
-   puts "5 SECOND(S)!"
-   num >= 5
-    end
-  end
-end  
+# while num > 0
+#   puts "5 SECOND(S)!"
+#   num >= 5
+#     end
+#   end
+# end  
